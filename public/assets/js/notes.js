5d4890bcd915a78bc416678c8843c37e62546389
AnthonyC233 <changanthony61@yahoo.com>
2022-04-22 22:55:16 -0400

    added lines in notes.js

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const path = require('path');
-const dbData = require('./');
+const fs = require("fs")
+const notesData = require('./db/db.json');
 
-app.use(express.static('public'))
+const api = require('./routes/index.js');
 
-app.get('/api/notes', (req,res) => res.json(dbData));
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
